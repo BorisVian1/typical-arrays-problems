8a8edc0f2da59abacd98b042861e0a4b5bee979e
BorisVian1 <78557289+BorisVian1@users.noreply.github.com>
2021-09-03 11:55:51 +0400

    Update index.js
    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var num = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] > 0) {
+             num += array[i] / array.length;
+        }
+     }
+       return num;
 }
+       
